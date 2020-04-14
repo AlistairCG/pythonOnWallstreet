97c143df7df511b178bc1c256708b1213c101522
francescolosi <f.losi789@gmail.com>
2020-04-14 12:44:14 -0400

    runs until startService func

--- test_daemon.py
+++ test_daemon.py
@@ -43,10 +43,10 @@ import logzero
 from logzero import logger
 from signal import SIGTERM
 import socket
+
 import paramiko
 import threading
 import sys
-import signal
 import json
 import subprocess
 from stub_sftp import StubServer,  StubSFTPServer
@@ -54,7 +54,12 @@ from stub_sftp import StubServer,  StubSFTPServer
 host_key = paramiko.RSAKey(filename='test_rsa.key')
 logzero.logfile("soupLogFile.log", maxBytes=1e6, backupCount=2)
 
-   
+def getPid(pidfile, default=None):
+    try:
+        with open(pidfile) as f:
+            return int(f.read().strip())
+    except IOError:
+        return default
 def grimReaper(signalNumber,frame):
    while True:
         try:
@@ -69,13 +74,6 @@ def exitErr(msg,status=1):
    print(msg)
    sys.exit(status)
 
-def initDaemon():
-	os.chdir('/')
-	try:
-		os.setsid()
-	except:
-		pass
-	os.umask(0)
 class Daemon(object):
     def __init__(self, pidfile,stdin='/dev/null',stdout='/dev/null',stderr='/dev/null'):
       self.stdin=stdin
@@ -84,13 +82,16 @@ class Daemon(object):
       self.pidfile=pidfile
    
     def startService(self):
+        print("starting service")
         try:           #first fork
             if os.fork()>0:
                 raise SystemExit(0)
         except OSError as e:
             logger.info("First fork failed: %d (%s)"%(e.errno,e.strerror))
             
-        initDaemon()
+        os.chdir('/')
+        os.setsid()
+        os.setuid(1000)
             
         try:        #2nd fork
             if os.fork()>0:
@@ -100,23 +101,30 @@ class Daemon(object):
                 
         sys.stdout.flush()
         sys.stderr.flush()
-        si=open(self.stdin,'r')
-        so=open(self.stdout,'a+')
-        se=open(self.stderr,'a+')
-        os.dup2(si.fileno(),sys.stdin.fileno())
-        os.dup2(so.fileno(),sys.stdout.fileno())
-        os.dup2(se.fileno(),sys.stderr.fileno())
+        with open(self.stdin, 'rb', 0) as f:
+            os.dup2(f.fileno(), sys.stdin.fileno())
+        with open(self.stdout, 'ab', 0) as f:
+            os.dup2(f.fileno(), sys.stdout.fileno())
+        with open(self.stderr, 'ab', 0) as f:
+            os.dup2(f.fileno(),  sys.stderr.fileno())
+        #si=open(self.stdin,'r')
+        #so=open(self.stdout,'a+')
+        #se=open(self.stderr,'a+')
+        #os.dup2(si.fileno(),sys.stdin.fileno())
+        #os.dup2(so.fileno(),sys.stdout.fileno())
+        #os.dup2(se.fileno(),sys.stderr.fileno())
         
-        pid=str(os.getpid())
-        with open(self.pidfile, 'w+') as f:
-            f.write(pid)
+        #pid=str(os.getpid())
+
+        with open(self.pidfile, 'w') as f:
+            print(os.getpid(), file=f)
         
         atexit.register(lambda:os.remove(self.pidfile))
+        signal.signal(signal.SIGTERM,grimReaper)
         sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
         sock.bind(getBinds())
         sock.listen(100)
-        signal.signal(signal.SIGCHLD,grimReaper)
         print('[+] Listening for connection ...')
         while True:
             try:
@@ -135,19 +143,30 @@ class Daemon(object):
     
     
     def start(self):
-        if os.path.exists(self.pidfile):
+        print("starting daemon")
+        if getPid(self.pidfile):
             msg="Pidfile %s already exists. daemon already running"
             exitErr(msg%self.pidfile)
         self.startService()
         self.run()
    
     def stop(self):
-        if os.path.exists(self.pidfile):
-            with open(self.pidfile) as f:
-                os.kill(int(f.read()), signal.SIGTERM)
-                sys.exit(0)
-        else:
-            sys.exit(0)
+        pid=getPid(self.pidfile)
+        if not pid:
+            msg="Pidfile dont exist, daemon not running"
+            print(msg%self.pidfile)
+            return
+        try:
+            while True:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError as e:
+            error=e.strerror
+            if 'No such process' in error:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+                else:
+                    exitErr(error)
     
     def restart(self):
         self.stop()
@@ -247,7 +266,7 @@ if __name__=="__main__":
     logzero.logfile("/tmp/rotating-logfile.log",maxBytes=1e6, backupCount=3, disableStderrLogger=True)
     daemonPid=os.getpid()
     logger.info(f"Started {daemonPid}")
-    daemon=MyDaemon('/tmp/daemon-example24.pid')
+    daemon=MyDaemon('/tmp/daemon-example69.pid')
     if len(sys.argv)==2:
         if 'start'==sys.argv[1]:
             daemon.start()
